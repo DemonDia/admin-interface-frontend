6dd1bea7f0cd039bfe4266af30c06261b65d3810
Setsuna Yukiii <siang-meng@live.com>
2022-12-03 15:33:50 +0800

    layout for adding project page

---
+++ src/components/projects/ProjectForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProjectForm(props) {
+    return (
+        <div>
+            <h1>Project Form</h1>
+            
+        </div>
+    );
+}
+
+export default ProjectForm;
--- src/pages/projects/AddProject.js
+++ src/pages/projects/AddProject.js
@@ -1,10 +1,55 @@
-import React from 'react';
-
+import React, { useState, useEffect } from "react";
+import ProjectForm from "../../components/projects/ProjectForm";
+import { Link, useNavigate } from "react-router-dom";
+import { defaultAuthCheck } from "../../AuthCheck";
+import axios from "axios";
 function AddProject(props) {
+    const navigate = useNavigate();
+    const currentToken = localStorage.getItem("loginToken");
+    const userId = localStorage.getItem("userId");
+
+    const addUserProject = async (project) => {
+        await axios
+            .post(
+                process.env.REACT_APP_BACKEND_API + "/api/projects/add",
+                {
+                    ...project,
+                    userId,
+                },
+                { headers: { Authorization: `Bearer ${currentToken}` } }
+            )
+            .then((res) => {
+                if (res.data.success) {
+                    alert("Successfully added");
+                    navigate("/projects");
+                } else {
+                    alert("Failed to add");
+                }
+            })
+            .catch((err) => {
+                alert("Failed to add");
+            });
+    };
+    useEffect(() => {
+        defaultAuthCheck(navigate, axios);
+    }, []);
     return (
         <div>
             <h1>Add Project</h1>
-            
+            <nav aria-label="breadcrumb" className="breadcrumbContainer card">
+                <ol className="breadcrumb">
+                    <li className="breadcrumb-item">
+                        <Link to="/home">Home</Link>
+                    </li>
+                    <li className="breadcrumb-item" aria-current="page">
+                        <Link to="/projects">Project (cancel)</Link>
+                    </li>
+                    <li className="breadcrumb-item active" aria-current="page">
+                        Add new project
+                    </li>
+                </ol>
+            </nav>
+            <ProjectForm addItem={addUserProject} project={null} />
         </div>
     );
 }
