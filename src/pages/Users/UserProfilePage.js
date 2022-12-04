ab0581959f1acaf27234955566a1532699648893
Setsuna Yukiii <siang-meng@live.com>
2022-12-04 14:45:34 +0800

    adding of integration guide

--- src/pages/Users/UserProfilePage.js
+++ src/pages/Users/UserProfilePage.js
@@ -51,6 +51,7 @@ function UserProfilePage(props) {
                     </ol>
                 </nav>
                 <div className="card profileContainer">
+                    <h4>User information</h4>
                     <label>Name:</label>
                     <input
                         className="form-control"
@@ -60,7 +61,6 @@ function UserProfilePage(props) {
                         }}
                     />
                     <label>User ID (please do NOT share this):</label>
-
                     <div className="input-group">
                         <input
                             className="form-control"
@@ -77,7 +77,7 @@ function UserProfilePage(props) {
                             }}
                         >
                             <span
-                                class="input-group-text"
+                                className="input-group-text"
                                 id="basic-addon1"
                                 style={{ height: "100%" }}
                             >
@@ -90,7 +90,7 @@ function UserProfilePage(props) {
                             }}
                         >
                             <span
-                                class="input-group-text"
+                                className="input-group-text"
                                 id="basic-addon1"
                                 style={{ height: "100%" }}
                             >
@@ -108,6 +108,170 @@ function UserProfilePage(props) {
                         type="number"
                         disabled
                     />
+                </div>
+                <div className="card profileContainer">
+                    <h3>User guides</h3>
+                    <ul
+                        className="nav nav-tabs"
+                    >
+                        <li
+                            className="nav-item"
+                            id="integrationGuide"
+                            data-bs-toggle="pill"
+                            data-bs-target="#v-pills-integrationGuide"
+                            type="button"
+                            role="tab"
+                            aria-controls="v-pills-integrationGuide"
+                            aria-selected="true"
+                        >
+                            <Link className="nav-link" style = {{color:"#0011A7"}}>Integration Guide</Link>
+                            
+                        </li>
+                        <li
+                            className="nav-item"
+                            id="apiGuide"
+                            data-bs-toggle="pill"
+                            data-bs-target="#v-pills-apiGuide"
+                            type="button"
+                            role="tab"
+                            aria-controls="v-pills-apiGuide"
+                            aria-selected="false"
+                        >
+                           <Link className="nav-link" style = {{color:"#0011A7"}}>API Guide</Link>
+                        </li>
+                    </ul>
+
+                    <div className="tab-content" id="v-pills-tabContent">
+                        <div
+                            className="tab-pane fade show active"
+                            id="v-pills-integrationGuide"
+                            role="tabpanel"
+                            aria-labelledby="v-pills-integrationGuide"
+                            tabindex="0"
+                        >
+                            <ol className="list-group list-group-numbered guideSteps">
+                                <li className="list-group-item">
+                                    Create a .env file (This stores your
+                                    environment variables) in the root of your
+                                    site directory.
+                                </li>
+                                <li className="list-group-item">
+                                    Copy your user ID as show below.
+                                    <br></br>
+                                    <label>
+                                        User ID:{" "}
+                                        <b>(Please do NOT share this)</b>
+                                    </label>
+                                    <div className="input-group">
+                                        <input
+                                            className="form-control"
+                                            value={userId}
+                                            type={
+                                                !displayId ? "password" : "text"
+                                            }
+                                            onChange={(e) => {
+                                                setUserId(e.target.value);
+                                            }}
+                                            disabled
+                                        />
+                                        <Link
+                                            onClick={() => {
+                                                navigator.clipboard.writeText(
+                                                    userId
+                                                );
+                                            }}
+                                        >
+                                            <span
+                                                className="input-group-text"
+                                                id="basic-addon1"
+                                                style={{ height: "100%" }}
+                                            >
+                                                <CopyToClipbaord />
+                                            </span>
+                                        </Link>
+                                        <Link
+                                            onClick={() => {
+                                                setDisplayId(!displayId);
+                                            }}
+                                        >
+                                            <span
+                                                className="input-group-text"
+                                                id="basic-addon1"
+                                                style={{ height: "100%" }}
+                                            >
+                                                {!displayId ? (
+                                                    <MaskIcon />
+                                                ) : (
+                                                    <UnmaskIcon />
+                                                )}
+                                            </span>
+                                        </Link>
+                                    </div>
+                                </li>
+                                <li className="list-group-item">
+                                    Save it as "USER_ID" in your .env file. (Add
+                                    a "REACT_APP_" such that it is saved as
+                                    "REACT_APP_USER_ID" if your site is created
+                                    with React.js)
+                                </li>
+                                <li className="list-group-item">
+                                    Save the link below as "SERVER" in your .env
+                                    file. (Add a "REACT_APP_" such that it is
+                                    saved as "REACT_APP_SERVER" if your site is
+                                    created with React.js). This allows you to
+                                    fetch the data from your account into your
+                                    portfolio site.
+                                    <br></br>
+                                    <label>Server link:</label>
+                                    <div className="input-group">
+                                        <input
+                                            className="form-control"
+                                            value={
+                                                process.env
+                                                    .REACT_APP_BACKEND_API
+                                            }
+                                            disabled
+                                        />
+                                        <Link
+                                            onClick={() => {
+                                                navigator.clipboard.writeText(
+                                                    process.env
+                                                        .REACT_APP_BACKEND_API
+                                                );
+                                            }}
+                                        >
+                                            <span
+                                                className="input-group-text"
+                                                id="basic-addon1"
+                                                style={{ height: "100%" }}
+                                            >
+                                                <CopyToClipbaord />
+                                            </span>
+                                        </Link>
+                                    </div>
+                                </li>
+                                <li className="list-group-item">
+                                    Remember to put the variables as mentioned
+                                    into the environment variables on the
+                                    services you use to deploy your portfolio
+                                    sites!<br></br>
+                                </li>
+                                <li className="list-group-item">
+                                    Refer the API guide in the next tab and you
+                                    are all set!
+                                </li>
+                            </ol>
+                        </div>
+                        <div
+                            className="tab-pane fade"
+                            id="v-pills-apiGuide"
+                            role="tabpanel"
+                            aria-labelledby="v-pills-apiGuide"
+                            tabindex="0"
+                        >
+                            ...
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
